60d0b9b5325b740895274fe17a0947c90df6e743
karnakar <karnakar@ymail.com>
2020-09-01 01:20:15 -0400

    first

---
+++ src/main/java/com/ssk/model/SskServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ssk.model;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SskServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SskServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ssk/model/SskServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssk.model;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SskServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
