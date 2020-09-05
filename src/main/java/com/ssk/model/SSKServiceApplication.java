30dcabcefe09f75e403dde2ed4aaa9703e742880
karnakar <karnakar@ymail.com>
2020-09-05 05:11:23 -0400

    WAR file updates

--- src/main/java/com/ssk/model/SSKServiceApplication.java
+++ src/main/java/com/ssk/model/SSKServiceApplication.java
@@ -2,11 +2,20 @@ package com.ssk.model;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-@SpringBootApplication
-public class SSKServiceApplication {
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-	public static void main(String[] args) {
+@SpringBootApplication
+public class SSKServiceApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SSKServiceApplication.class);
+	}
+
+	public static void main(String[] args) throws Exception {
 		SpringApplication.run(SSKServiceApplication.class, args);
 	}
+
 
 }
